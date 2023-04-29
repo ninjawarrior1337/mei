29152ef3fdd10cf7492b27b083a18ba2e8d06cef
ninjawarrior1337 <me@treelar.xyz>
2023-04-29 12:39:17 -0700

    move wasm module to get file size down

--- src/mei_native.ts
+++ src/mei_native.ts
@@ -1,8 +1,8 @@
-import wasm from "../native/pkg/mei_native_bg.wasm"
+// import wasm from "../native/pkg/mei_native_bg.wasm"
 import * as pkg from "../native/pkg"
 
 export const useNativeCode = async () => {
-    await pkg.default(wasm)
+    await pkg.default("https://fs.treelar.xyz/mei/mei_native_bg.wasm")
 
     return pkg
 }
