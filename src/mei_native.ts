338156e3a04ed0f7f3d737c2cc49085b222fb934
ninjawarrior1337 <me@treelar.xyz>
2023-05-01 13:34:51 -0700

    cool it works now

--- src/mei_native.ts
+++ src/mei_native.ts
@@ -1,9 +1,6 @@
-//@ts-ignore
-import wasm from "../native/pkg/mei_native_bg.wasm?module"
+// import wasm from "../native/pkg/mei_native_bg.wasm"
 
 import * as pkg from "../native/pkg"
 export const useNativeCode = async () => {
-    await pkg.default(wasm)
-
     return pkg
 }
--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,26 +1,18 @@
-import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
+import { createNextApiHandler } from '@trpc/server/adapters/next';
 import { NextRequest } from 'next/server';
 import { createContext } from '~/trpc/context';
 import { appRouter } from '~/trpc/router';
 
-export const config = {
-    runtime: 'edge',
-}
-
-export default async function handler(req: NextRequest) {
-    return fetchRequestHandler({
-        endpoint: '/api/trpc',
-        router: appRouter,
-        req,
-        createContext,
-        responseMeta(opts) {
-            const ONE_HOUR_IN_SECONDS = 60 * 60;
-            const ONE_DAY_IN_SECONDS = ONE_HOUR_IN_SECONDS * 24;
-            return {
-                headers: {
-                    'cache-control': `s-maxage=${ONE_HOUR_IN_SECONDS}, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
-                },
-            }
+export default createNextApiHandler({
+    router: appRouter,
+    createContext,
+    responseMeta(opts) {
+        const ONE_HOUR_IN_SECONDS = 60 * 60;
+        const ONE_DAY_IN_SECONDS = ONE_HOUR_IN_SECONDS * 24;
+        return {
+            headers: {
+                'cache-control': `s-maxage=${ONE_HOUR_IN_SECONDS}, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
+            },
         }
-    });
-}
+    }
+})
