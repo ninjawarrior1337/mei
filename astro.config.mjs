0909aab7d1c6a730b761d7c012c051642c49d6c5
ninjawarrior1337 <me@treelar.xyz>
2023-05-02 00:17:42 -0700

    fix tailwind not working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from "@astrojs/vercel/serverless"
-import wasm from "vite-plugin-wasm"
-import topLevelAwait from "vite-plugin-top-level-await"
 
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
   adapter: vercel(),
-  vite: {
-    plugins: [
-      wasm(),
-      // topLevelAwait()
-    ]
-  }
 });
