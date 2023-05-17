268c18e8e0f7d58bb897a6b994a37d9656695f44
ninjawarrior1337 <me@treelar.xyz>
2023-05-17 14:28:58 -0700

    add cors to options method

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -4,9 +4,18 @@ import { createContext } from "~/server/trpc/context";
 import { appRouter } from '~/server/trpc/router';
 
 export const all: APIRoute = (opts) => {
+    const CORS = {
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+        "Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    }
     if (opts.request.method === 'OPTIONS') {
         return new Response(null, {
-            status: 200
+            status: 200,
+            headers: {
+                ...CORS
+            }
         })
     }
     return fetchRequestHandler({
@@ -17,12 +26,7 @@ export const all: APIRoute = (opts) => {
         responseMeta(opts) {
             const ONE_HOUR_IN_SECONDS = 60 * 60;
             const ONE_DAY_IN_SECONDS = ONE_HOUR_IN_SECONDS * 24;
-            const CORS = {
-                "Access-Control-Allow-Credentials": "true",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-                "Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-            }
+            
             return {
                 headers: {
                     ...CORS,
