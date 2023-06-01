e6ad4f77d9cd036b9f5f6a05bad9b0bbc9ea4b45
ninjawarrior1337 <me@treelar.xyz>
2023-05-31 23:52:51 -0700

    write ffmpeg glue code

--- native/src/main.rs
+++ native/src/main.rs
@@ -1,9 +1,11 @@
 use std::{
     fs::{self, File},
-    io::Write,
+    io::{Write, stdin, Read, Cursor, BufReader, BufRead},
 };
 
+use image::ImageFormat;
 use indicatif::{ParallelProgressIterator, ProgressIterator};
+use itertools::Itertools;
 use rayon::prelude::*;
 
 use mei_native::computer_craft::{
@@ -79,6 +81,29 @@ fn write_bimg() {
 }
 
 fn main() {
-    write_rawmode();
+    let mut f = File::create("bocchi_ffmpeg.32").unwrap();
+    let i = stdin();
+    let mut reader = BufReader::new(i);
+
+    while reader.fill_buf().map(|v| !v.is_empty()).unwrap() {
+        let mut img_buf = image::RgbImage::new(1280, 720);
+
+        let p = img_buf.pixels_mut();
+        p.for_each(|p| {
+            let mut pix_buf = [0u8; 3];
+            reader.read_exact(&mut pix_buf).unwrap();
+            p.0 = pix_buf
+        });
+
+        let o = ImageRenderer::new(51, 19)
+            .image(img_buf.into())
+            .quantizer(ImageQuant::new_with_opts(Some(10), Some(image::imageops::FilterType::Nearest)))
+            .serializer(RawModePacketSerializer)
+            .render();
+
+        f.write(&o);
+    }
+
+    // write_rawmode();
     // write_bimg();
 }
