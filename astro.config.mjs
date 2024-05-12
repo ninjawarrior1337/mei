c4959e6d8452092742042c44cda6c0e8c7d6048c
ninjawarrior1337 <me@treelar.xyz>
2024-05-11 22:33:16 -0700

    enable isr

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,11 @@ import wasm from 'vite-plugin-wasm';
 export default defineConfig({
   integrations: [vue(), tailwind()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    isr: {
+      expiration: 60
+    },
+  }),
   vite: {
     plugins: [
       wasm()
