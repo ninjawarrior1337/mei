f998a1d950a0a0315b37a6ca29aff8237066524d
Treelar <me@treelar.xyz>
2026-03-08 11:37:04 -0700

    try to fix the vercel build

--- build.sh
+++ build.sh
@@ -2,14 +2,15 @@
 
 echo "Installing Rustup..."
 # Install Rustup (compiler)
+export RUSTUP_INIT_SKIP_PATH_CHECK=yes
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 # Adding binaries to path
 . "/rust/env"
 
 echo "Installing wasm-pack..."
 # Install wasm-pack
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
-
+curl https://drager.github.io/wasm-pack/installer/init.sh -sSf | sh
 
 echo "Starting native build"
 pnpm native-build
