ac7a5a8016c6b95f2807140825287da380568a71
Treelar <me@treelar.xyz>
2026-03-08 11:18:52 -0700

    fix buildscript

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ echo "Installing Rustup..."
 # Install Rustup (compiler)
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 # Adding binaries to path
-source "$HOME/.cargo/env"
+. "/rust/env"
 
 echo "Installing wasm-pack..."
 # Install wasm-pack
